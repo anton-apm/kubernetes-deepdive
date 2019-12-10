e726f195d1513077130e62d9d94a079897af6ce5
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2019-12-10 12:10:53 +0100

    fix tls

--- activegate/fix-tls.sh
+++ activegate/fix-tls.sh
@@ -2,7 +2,9 @@
 #!/bin/bash
 
 # We copy the custom.properties to the default path.
+printf "copying custom.properties file to /var/lib/dynatrace/gateway/config/custom.properties"
 cp custom.properties /var/lib/dynatrace/gateway/config/custom.properties
-
+printf "restarting dynatrace gateway.."
 # We restart the service
 service dynatracegateway restart
+printf "done restarting dynatrace"
