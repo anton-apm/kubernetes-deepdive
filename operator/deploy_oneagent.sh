f8b3665030036b35bea085208038cb548c44d405
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-01-03 15:51:39 +0100

    download kubernetes.yaml

--- operator/deploy_oneagent.sh
+++ operator/deploy_oneagent.sh
@@ -29,7 +29,8 @@ echo "Creating dynatrace K8s namespace"
 kubectl create namespace dynatrace
 LATEST_RELEASE=$(curl -s https://api.github.com/repos/dynatrace/dynatrace-oneagent-operator/releases/latest | grep tag_name | cut -d '"' -f 4)
 echo "Creating K8s deployment for the latest oneagent operator release"
-kubectl create -f https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/$LATEST_RELEASE/deploy/kubernetes.yaml
+curl -o kubernetes.yaml https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/$LATEST_RELEASE/deploy/kubernetes.yaml
+kubectl create -f kubernetes.yaml
 #kubectl -n dynatrace logs -f deployment/dynatrace-oneagent-operator
 kubectl -n dynatrace create secret generic oneagent --from-literal="apiToken=$API_TOKEN" --from-literal="paasToken=PAAS_TOKEN"
 
