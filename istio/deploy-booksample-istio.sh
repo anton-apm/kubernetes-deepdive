0d8946f41549e518eb1751a469a83713dbc2dcb7
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-01-07 09:54:17 +0100

    adding the url of the book app in istio

--- istio/deploy-booksample-istio.sh
+++ istio/deploy-booksample-istio.sh
@@ -5,8 +5,8 @@
 # We enable istio and when prompted if enable TLS authentication between sidecars injection we say 'N'o
 echo 'N;' | microk8s.enable istio
 
-# TODO printout the http NodePort of the Istio IngressGateway with the URL of the app.
-export PUBLIC_IP=$(curl -s ifconfig.me)
+echo "We sleep for a minute to give time to istio to initiate..." 
+sleep 1m
 
 # We create a namespace for the application
 kubectl create ns book
@@ -15,4 +15,7 @@ kubectl label namespace book istio-injection=enabled
 # we install the book 
 kubectl -n book apply -f . 
 
-echo "The Book application is now available at http://$PUBLIC_IP :[http IstioIngressGwNodePort]/productpage"
+# We print out the http NodePort of the Istio IngressGateway with the URL of the app.
+export PUBLIC_IP=$(curl -s ifconfig.me)
+nodePort=$(kubectl get svc -n istio-system istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+printf "\n\n*****\nThe Book application is now available at http://$PUBLIC_IP:$nodePort/productpage\n*******\n\n"
